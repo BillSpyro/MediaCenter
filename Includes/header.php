cdd9ee0c0bdb426cd81183bfc025988cb8c83e23
Melaku <>
2021-02-24 08:58:04 -0500

    Create an auth folder for login and register page create a basic register form in register page create a register inc file for php code

--- Includes/header.php
+++ Includes/header.php
@@ -2,15 +2,10 @@
 <header>
     <nav>
         <ul>
-          <?php session_start();
-          if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true): ?>
-            <li><a href="../Accounts/logout.php">Log Out</a></li>
-            <li><a href="../Accounts/accountPage.php">Home</a></li>
-          <?php else: ?>
-            <li><a href="../Accounts/index.php">Home</a></li>
-            <li><a href="../Accounts/registerPage.php">Register</a></li>
-            <li><a href="../Accounts/loginPage.php">Log In</a></li>
-          <?php endif ?>
+ 
+            <li><a href="index.php">Home</a></li>
+            <li><a href="auth/register.php">Register</a></li>
+            <li><a href="auth/login.php">Log In</a></li>
         </ul>
     </nav>
 </header>
---
+++ Includes/register_inc.php
@@ -0,0 +1,8 @@
+<?php
+
+
+if(isset($_POST["submit"])) {
+    echo "yeay";
+}else {
+    echo "noonon";
+}
