a247e700be450db0d1f19b0a900202465a758c66
Charles Max Crandall <robloxatme.com@gmail.com>
2021-03-01 09:19:19 -0500

    Added basic profile viewing

--- homepage/index.php
+++ homepage/index.php
@@ -18,13 +18,20 @@ include "../includes/header.php";
 <section class="home">
 <h1>Hi Welcome to our site!</h1>
 
-
+<?php if (!isset($_SESSION['id'])):?>
    <div class="home-log">
        <p>Have an account? Log in.</p>
 
         <li><a href="../auth/login.php">Log In</a></li>
         <p>Don't have an account? register.</p>
         <li><a href="../auth/register.php">Register</a></li>
+<?php else: ?>
+<li><a href="../profile/profile.php?ID=<?php echo $_SESSION['id']?>">Your Profile</a></li>
+
+<?php endif ?>
+
+
+
 </div>
 </section>
 <!-- include footer page -->
