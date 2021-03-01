95c097fdb12d4393594d306a9fd3a5e4f083adfa
Eric Moyer <ejmoyer3@comcast.net>
2021-03-01 11:17:46 -0500

    Post include done, this page can be included on any page where posting is available. It will allow the user to create a new post.

---
+++ includes/post_inc.php
@@ -0,0 +1,22 @@
+<?php
+include_once "header.php";
+// check if they are logged in, if not send them to the home page
+if (!$_SESSION['id']) {
+  exit();
+  header("Location: homepage/index.php");
+}
+?>
+
+<form action="includes/post_handler.php" method="post">
+  <label for="postName">Title:</label>
+  <input type="text" name="postName" required>
+
+  <label for="postContent">Content:</label>
+  <input type="text" name="postContent" required>
+
+  <input type="submit">
+</form>
+
+<?php
+include_once "footer.php";
+?>
