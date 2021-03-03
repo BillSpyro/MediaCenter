657500d4bb96207df86f3233a465dc3f7c1e43d7
Eric Moyer <ejmoyer3@comcast.net>
2021-03-03 08:01:33 -0500

    Fixed filepath to post_handler for use in other pages.

--- includes/post_handler.php
+++ includes/post_handler.php
@@ -15,6 +15,7 @@ if (isset($_POST['postName']) && isset($_POST['postContent'])) {
     $stmt->execute();
     $stmt->close();
     mysqli_close($conn);
+    // header("Location: link to user's feed goes here");
   }
 }
 ?>
