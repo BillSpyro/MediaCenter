1ae32fe683c110b624d6377a00721ecb5e0770fc
Eric Moyer <ejmoyer3@comcast.net>
2021-03-08 10:24:11 -0500

    Changed SQL file to include a datetime column, removed not null constraints on postid, commentid, profileid. Added lines of code to posts.php to display any comments the posts may have.

--- profile/posts.php
+++ profile/posts.php
@@ -4,6 +4,7 @@ include_once "../includes/dbc_inc.php";
 
 // sql here for all posts in descending order
 if ($_SESSION['id']) {
+  $userId;
 // SELECT name, content, post_time, likes, dislikes, reposts FROM posts WHERE user_ID = ? ORDER BY post_time DESC
   $query = "SELECT id, name, content, post_time, likes, dislikes, reposts FROM posts WHERE user_ID = ? ORDER BY post_time DESC;";
   if ($stmt = $conn->prepare($query)) {
@@ -14,12 +15,29 @@ if ($_SESSION['id']) {
       printf(<<<EOT
       <section>
         <h1>%s</h1>
-        <p>%s</p>
+        <p>Posted: %s</p>
         <p>%s</p>
         <p>Likes: %s Dislikes: %s Reposts: %s</p>
         <a href="edit_post.php?postid=%s">Edit</a>
+        <p>Comments</p>
       </section>
       EOT, $name, $postTime, $content, $likes, $dislikes, $reposts, $id);
+      $userId = $id;
+    }
+    // query for comments
+    $query = "SELECT username, posted_date, content FROM comments JOIN users ON users.id = comments.user_ID WHERE post_ID = ? ORDER BY posted_date DESC;";
+    if ($stmt = $conn->prepare($query)) {
+      $stmt->bind_param("i", $userId);
+      $stmt->execute();
+      $stmt->bind_result($username, $posted_date, $content);
+      // display the comments
+      while ($stmt->fetch()) {
+        printf(<<<EOT
+        <p>%s</p>
+        <span>%s</span>
+        <p>%s</p>
+        EOT, $username, $posted_date, $content);
+      }
     }
   }
 }
