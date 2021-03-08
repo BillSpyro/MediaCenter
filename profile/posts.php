66bd1352332216c15e6118b2156f30a55fc50a9d
Eric Moyer <ejmoyer3@comcast.net>
2021-03-08 11:03:28 -0500

    Added some extra section tags to comments, changed the wording on comments to sound more proper, made an area to create comments and made the code to create comments.

--- profile/posts.php
+++ profile/posts.php
@@ -4,7 +4,7 @@ include_once "../includes/dbc_inc.php";
 
 // sql here for all posts in descending order
 if ($_SESSION['id']) {
-  $userId;
+  $postId;
 // SELECT name, content, post_time, likes, dislikes, reposts FROM posts WHERE user_ID = ? ORDER BY post_time DESC
   $query = "SELECT id, name, content, post_time, likes, dislikes, reposts FROM posts WHERE user_ID = ? ORDER BY post_time DESC;";
   if ($stmt = $conn->prepare($query)) {
@@ -22,20 +22,36 @@ if ($_SESSION['id']) {
         <p>Comments</p>
       </section>
       EOT, $name, $postTime, $content, $likes, $dislikes, $reposts, $id);
-      $userId = $id;
-    }
+
+      // create comments
+      printf(<<<EOT
+      <section>
+        <form action="../includes/create_post_comment.php" method="POST">
+          <input type="text" name="postId" value="%s" hidden>
+          <label for='comment'>New Comment</label>
+          <input type='text' name='comment'>
+          <input type='submit'>
+        </form>
+      </section>
+      EOT, $id);
+
+      // used for query for finding comments
+      $postId = $id;
+  }
+
     // query for comments
     $query = "SELECT username, posted_date, content FROM comments JOIN users ON users.id = comments.user_ID WHERE post_ID = ? ORDER BY posted_date DESC;";
     if ($stmt = $conn->prepare($query)) {
-      $stmt->bind_param("i", $userId);
+      $stmt->bind_param("i", $postId);
       $stmt->execute();
       $stmt->bind_result($username, $posted_date, $content);
       // display the comments
       while ($stmt->fetch()) {
         printf(<<<EOT
-        <p>%s</p>
-        <span>%s</span>
-        <p>%s</p>
+        <section>
+          <p>%s commented on <span>%s:</span></p>
+          <p>%s</p>
+        <section>
         EOT, $username, $posted_date, $content);
       }
     }
