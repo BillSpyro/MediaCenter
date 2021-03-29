ac67c46d51cfb50a397d04f5fda458bd2ad99744
Charles Max Crandall <robloxatme.com@gmail.com>
2021-03-29 10:54:06 -0400

    There will be a notification to a users friends when the user updates their profile

--- includes/update_inc.php
+++ includes/update_inc.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once 'dbc_inc.php';
+include_once "create_notification_inc.php";
+
 session_start();
 if (isset($_POST["submit"])) {
     $profile_picture = '../images/' . $_FILES['image']['name'];
@@ -16,7 +18,7 @@ if (isset($_POST["submit"])) {
     $education = $_POST["education"];
     $relation = $_POST["relation"];
 
-    
+
 // update profile
 
 $user_ID = $_SESSION['id'];
@@ -33,11 +35,29 @@ $sql = "UPDATE profile SET  profile_picture = ?, first_name = ?, middle_name = ?
     mysqli_stmt_bind_param($stmt, "sssssssssssss", $profile_picture, $firstname, $middle, $lastname, $phone, $date, $gender, $description, $location,  $job, $education, $relation, $user_ID);
 
     mysqli_stmt_execute($stmt);
+
+    //Getting ID of the profile that was updated
+    $query = "SELECT * FROM profile WHERE user_ID = ?";
+    if ($stmt = $conn->prepare($query)) {
+
+      $stmt->bind_param("i", $user_ID);
+
+      $stmt->execute();
+
+      $result = $stmt->get_result();
+
+      while ($row = $result->fetch_array()) {
+        $profile_ID = $row['id'];
+      }
+    }
+
     mysqli_stmt_close($stmt);
+
+    createNotification('Update Profile', $user_ID, $profile_ID);
     header("location: ../profile/profile.php?ID=$user_ID");
         exit();
 
- 
+
 }
 }
 
