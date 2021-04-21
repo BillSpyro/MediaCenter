fbc9583c2e5642d1183a431b1ea84530a95f2aea
Eric Moyer <ejmoyer3@comcast.net>
2021-04-21 08:49:53 -0400

    Spaced out pages that deal with editting posts. Added a few comments for easier understanding.

--- includes/edit_post_inc.php
+++ includes/edit_post_inc.php
@@ -1,16 +1,20 @@
 <?php
 session_start();
 include_once "dbc_inc.php";
-
+// get all the info given by edit_post.php and update the Posts table
 if (isset($_POST['postId']) && isset($_POST['postName']) && isset($_POST['postContent'])) {
   $postId = $_POST['postId'];
   $query = "UPDATE posts SET name = ?, content = ? WHERE id = ?;";
+  
   if ($stmt = $conn->prepare($query)) {
     $stmt->bind_param("ssi", $_POST['postName'], $_POST['postContent'], $_POST['postId']);
+
     $stmt->execute();
+
     $stmt->close();
   }
 }
 mysqli_close($conn);
+
 header("Location: ../homepage/index.php#$postId");
 ?>
--- posts/edit_post.php
+++ posts/edit_post.php
@@ -3,19 +3,25 @@ include_once "../includes/header.php";
 include_once "../includes/dbc_inc.php";
 
 if ($_SESSION['id']) {
+  // set some empty variables, populating them later with older data
   $postName;
   $postContent;
   $query = "SELECT name, content FROM posts WHERE user_ID = ? AND id = ?;";
+
   if ($stmt = $conn->prepare($query)) {
     $stmt->bind_param("ii", $_SESSION['id'], $_GET['postid']);
+
     $stmt->execute();
+
     $stmt->bind_result($name, $content);
+    
     while ($stmt->fetch()) {
       $postName = $name;
       $postContent = $content;
     }
   }
 }
+// Show this old data in the editting boxes
 ?>
 
 <section>
