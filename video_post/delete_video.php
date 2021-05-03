bc3fc4f8155d5fce6d97fef195ee764648170ce4
Melaku <>
2021-05-03 11:01:20 -0400

    fix bugs

--- video_post/delete_video.php
+++ video_post/delete_video.php
@@ -5,10 +5,20 @@ include_once '../includes/header.php';
 
 if(isset($_POST["delete_video"])) {
     $video_id = $_POST["video_id"];
-    $sql = "DELETE  FROM  videos WHERE id = ?";
+    $sql = "DELETE FROM  videos WHERE id = ?";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: update_video.php?error=stmtfaild");
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt, "s",  $video_id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    $sql_del = "DELETE FROM  comments WHERE video_ID = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql_del)) {
+        header("location: delete_video.php?error=stmtfaild");
         exit();
     }
     mysqli_stmt_bind_param($stmt, "s",  $video_id);
--- video_post/update_video.php
+++ video_post/update_video.php
@@ -6,6 +6,7 @@ include_once '../includes/header.php';
 $query = "SELECT * FROM profile, videos WHERE videos.user_ID = profile.user_ID ORDER BY post_time DESC";
 $result = $conn->query($query);
 while ($row = $result->fetch_array()) {
+    $video_id = $row["id"];
     $desc = $row["description"];
     $video_name = $row["links"];
 }
@@ -13,11 +14,12 @@ while ($row = $result->fetch_array()) {
 ?>
 <!-- update vidoe form -->
 <div class="update_video">
-    <form class="post-form " action="update_video.php" method="post" enctype="multipart/form-data">
+    <form class="post-form "  action="update_video.php" method="post" enctype="multipart/form-data">
         <p>Update Video</p>
         <hr>
-        <input class="body" type="text" name="description" value="<?php echo $desc?>" >
-        <input class="video" type="file" name="video"  value="<?php echo $video_name?>" >
+        <input id="update_video_id" type="text" name="video_id" value="<?php echo $video_id?>" hidden>
+        <input class="body" type="text" name="description" value="<?php echo $desc?>" ><br>
+        <input class="video" type="file" name="video"  value="<?php echo $video_name?>" ><br>
         <input class="submit" type="submit" name="submit" >
     </form>
 </div>
@@ -25,6 +27,7 @@ while ($row = $result->fetch_array()) {
 <!-- Update video -->
 <?php
 if (isset($_POST["submit"])) {
+    $video_id = $_POST["video_id"];
     $description = $_POST["description"];
     $video = '../images/' . $_FILES['video']['name'];
     $date = date('Y-m-d H:i:s');
@@ -32,13 +35,13 @@ if (isset($_POST["submit"])) {
 
 if(copy($_FILES['video']['tmp_name'], $video)) {
 
-    $sql = "UPDATE videos SET  description = ?, links = ?, post_time = ? WHERE user_ID = ?";
+    $sql = "UPDATE videos SET  description = ?, links = ?, post_time = ? WHERE id = ?";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: update_video.php?error=stmtfaild");
         exit();
     }
-    mysqli_stmt_bind_param($stmt, "ssss", $description, $video, $date, $user_ID);
+    mysqli_stmt_bind_param($stmt, "ssss", $description, $video, $date, $video_id);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
     header("location: videos.php?ID=$user_ID");
